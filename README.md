810f67c98b3c32c0b674e09c63ed14b64d069662
ungra <junhee_lee2@tmax.co.kr>
2024-11-18 23:31:37 +0900

    install firebase & apply github auth

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # lotto-nextjs14-app
-lotto app using nextjs, typescript, react, firebase, modules.css
+lotto app using nextjs, typescript, react, firebase, modules.css, vercel
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import { GithubAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../firebase";
+
 export default function AppMain() {
-  return <h1>AppMain Page</h1>;
+  const onClick = async () => {
+    try {
+      const provider = new GithubAuthProvider();
+      await signInWithPopup(auth, provider);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>AppMain Page</h1>
+      <button onClick={onClick}>Continue with Github</button>
+    </div>
+  );
 }
