684b836c850ad89159b257cb0a784c323f4e360f
ungra <ungrarhythm@naver.com>
2024-11-27 14:01:25 +0900

    Log-out

--- app/lotto/page.tsx
+++ app/lotto/page.tsx
@@ -1,26 +1,38 @@
 "use client";
 
-import { onAuthStateChanged, User } from "firebase/auth";
+import { onAuthStateChanged, signOut, User } from "firebase/auth";
 import { useEffect, useState } from "react";
 import { auth } from "../../firebase";
 import { useRouter } from "next/navigation";
 
 export default function Lotto() {
   const [user, setUser] = useState<User | null>(null);
+  const router = useRouter();
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
       if (currentUser) {
         setUser(currentUser);
       } else {
-        const router = useRouter();
         router.push("/login");
       }
     });
   }, []);
+  const onClick = async () => {
+    try {
+      const ok = confirm("Are you sure you want to log out?");
+      if (ok) {
+        await signOut(auth);
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div>
       <h1>Lotto Page...</h1>
       <h3>You Logged In</h3>
+      <button onClick={onClick}>Log Out</button>
     </div>
   );
 }
